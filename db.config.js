eb663a86502bc983da856ce59742cb8915be83b7
imvikaskashyap <mvikaskashyap@gmail.com>
2023-04-07 12:29:47 +0530

    first commit

---
+++ db.config.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+	"mongodb+srv://namespy6:6spyname@cpd.zrditb1.mongodb.net/test"
+);
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("./db.config");
+const User = require("./User");
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.get("/users/:userId", async (req, res) => {
+	try {
+		const user = await User.findOne({
+			userId: req.params.userId,
+		}).lean();
+		if (!user) {
+			res.status(404).json({ message: "User not found" });
+			return;
+		}
+		const { _id, ...rest } = user;
+		const userWithTimestamp = {
+			...rest,
+			timestamp: _id.getTimestamp(),
+		};
+		res.json(userWithTimestamp);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`server started on port ${PORT}`);
+});
